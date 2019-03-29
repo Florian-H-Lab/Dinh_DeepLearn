435c8c7d8119d111bbf893596fbaa6aa2420d139
Florian-H-Lab <f.heyl@gmx.de>
2019-03-29 17:02:44 +0100

    Changes to count_binding_sites_occur_in_RBPs.sh

--- bin/count_binding_sites_occur_in_RBPs.sh
+++ bin/count_binding_sites_occur_in_RBPs.sh
@@ -9,16 +9,20 @@
 # use script to count how often a binding site appears in different RBPs
 INPATH="/scratch/bi03/heylf/Dinh/peakcalling"
 OUT="/scratch/bi03/heylf/Dinh/RBPs_count"
-FILES=" "
+#FILES=" "
+
+echo "Generate First Intersect"
+for i in {1..167}; do
+	#FILES="$FILES $INPATH/Protein_rep"$i"_peaks_extended.bed" 
+	echo $i 
+	bedtools intersect -a "$INPATH/Protein_rep"$i"_peaks_extended.bed" -b "$INPATH/Protein_rep1_peaks_extended.bed" -s -c -f 0.5 > "$OUT/Protein_rep"$i"_RBP_count.bed"
+done
 
 for i in {1..167}; do
-	FILES="$FILES $INPATH/Protein_rep"$i"_peaks_extended.bed" 
+	for j in {2..167}; do
+		echo $i
+		echo $j 
+		bedtools intersect -a "$OUT/Protein_rep"$i"_RBP_count.bed" -b "$OUT/Protein_rep"$j"_RBP_count.bed" -s -c -f 0.5 > "$OUT/Protein_rep"$i"_RBP_count_tmp.bed"
+		mv "$OUT/Protein_rep"$i"_RBP_count_tmp.bed" "$OUT/Protein_rep"$i"_RBP_count.bed"
+	done
 done
-
-for j in {1..167}; do
-	echo $j 
-	bedtools intersect -a "$INPATH/Protein_rep"$j"_peaks_extended.bed" -b $FILES -s -c > "$OUT/Protein_rep"$j"_RBP_count.bed"
-done
-
-
-
